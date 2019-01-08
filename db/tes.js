4fb32ffc223e8bc3e51f898c400c4c25de1be78b
Tigran <tigranium123@gmail.com>
2019-01-08 16:36:30 +0300

    client side

---
+++ db/tes.js
@@ -0,0 +1,26 @@
+var bks=  [
+    {
+        idbooks: "1",
+        BookName: "First",
+        Price: "11",
+        Images: "/uploads/1.jpg"
+    },
+    {
+        idbooks: "2",
+        BookName: "Sec",
+        Price: "22",
+        Images: "/uploads/1.jpg"
+    }, {
+        idbooks: "3",
+        BookName: "Th",
+        Price: "33",
+        Images: "/uploads/1.jpg"
+    }, {
+        idbooks: "4",
+        BookName: "F",
+        Price: "44",
+        Images: "/uploads/1.jpg"
+    },
+
+];
+module.exports=bks;
