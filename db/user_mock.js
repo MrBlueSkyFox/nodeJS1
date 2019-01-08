1ea5ca5bee7a42ce183f80578e058b976bfae4ab
Tigran <tigranium123@gmail.com>
2019-01-08 21:51:51 +0300

    client side

--- db/user_mock.js
+++ db/user_mock.js
@@ -1,19 +1,19 @@
 var user_bk = [
     {
         id: "4",
-        name: "First",
+        name: "Joe",
         password: "$2a$10$8mN2RjES9TkZQ0BDiX9Swu8Lm./0x/Lty5pjJ4ioDQssJZNDAOaXq",
         _password: "fourth"
     },
     {
         id: "5",
-        name: "Second",
+        name: "Edward",
         password: "$2a$10$rtIb2oTc0bkypjksTK.rfee8hx3ICBP4syX1VKHXlr8zJuo8dWqem",
         _password: "five"
     },
     {
         id: "6",
-        name: "Third",
+        name: "Bob",
         password: "$2a$10$s8U3nC.Q.g7vPWZ0F2NMjupBRH8jhA0KWNKIaMl1UTc6OXE1GG4Uy",
         _password: "six"
     },
